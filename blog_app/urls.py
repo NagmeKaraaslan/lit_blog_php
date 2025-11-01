e07903252b39393e6455c3ce23f2e29c702df8fc
NagmeKaraaslan <karaaslannagme@gmail.com>
2025-11-01 18:29:25 +0300

    Removed all .py/django for php migration

--- blog_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PostListView, new_post
-
-app_name = 'blog_app'
-
-urlpatterns = [
-    path('', PostListView.as_view(), name='main_page'),
-    path('new-post/', new_post, name='new_post'),
-]
